fc5e453007c2662005968721332d2c77454d1909
Avery Black <avery.c.black@gmail.com>
2021-04-29 01:04:58 -0700

    This took way *way* too long to figure out... Leaving this here because this debug info could be useful

--- ECEnabler/ECEnabler.cpp
+++ ECEnabler/ECEnabler.cpp
@@ -3,10 +3,17 @@
 #include "ECEnabler.hpp"
 
 static const char *kextACPIEC[] { "/System/Library/Extensions/AppleACPIPlatform.kext/Contents/PlugIns/AppleACPIEC.kext/Contents/MacOS/AppleACPIEC" };
+static const char *kextACPIPlatform[] { "/System/Library/Extensions/AppleACPIPlatform.kext/Contents/MacOS/AppleACPIPlatform" };
 
 static KernelPatcher::KextInfo kextList[] {
     {"com.apple.driver.AppleACPIEC", kextACPIEC, arrsize(kextACPIEC), {true}, {}, KernelPatcher::KextInfo::Unloaded },
+    {"com.apple.driver.AppleACPIPlatform", kextACPIPlatform, arrsize(kextACPIPlatform), {true}, {}, KernelPatcher::KextInfo::Unloaded },
 };
+
+static UInt8 movPatchFind[] = {0x49, 0x89, 0x06,    // MOV (%R14), RAX
+                               0xeb, 0x34};         // JMP 0x36
+static UInt8 movPatchReplace[] = {0x41, 0x88, 0x06, // MOV (%R14), %AL
+                                  0xeb, 0x34};      // JMP 0x36
 
 static ECE *callbackECE = nullptr;
 
@@ -26,15 +33,63 @@ void ECE::deinit()
 
 void ECE::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size)
 {
-    if (index != kextList[0].loadIndex) return;
     SYSLOG("ECE", "process kext called! Index: %d Addr: %p Size: %x", index, address, size);
-    
-    KernelPatcher::RouteRequest request ("__ZN11AppleACPIEC14ecSpaceHandlerEjmjPmPvS1_", ecSpaceHandler, orgACPIEC_ecSpaceHandler);
-    
-    if (!patcher.routeMultiple(index, &request, 1, address  , size)) {
-        SYSLOG("ECE", "patcher.routeMultiple for %s failed with error %d", request.symbol, patcher.getError());
-        patcher.clearError();
+    if (index == kextList[0].loadIndex) {
+        KernelPatcher::RouteRequest request ("__ZN11AppleACPIEC14ecSpaceHandlerEjmjPmPvS1_", ecSpaceHandler, orgACPIEC_ecSpaceHandler);
+        
+        if (!patcher.routeMultiple(index, &request, 1, address  , size)) {
+            SYSLOG("ECE", "patcher.routeMultiple for %s failed with error %d", request.symbol, patcher.getError());
+            patcher.clearError();
+        }
+        
+        const KernelPatcher::LookupPatch movPatch = {
+            &kextList[0],
+            movPatchFind, // MOV (%R14),RAX
+            movPatchReplace, // MOV (%R14),AL
+            sizeof(movPatchFind),
+            1
+        };
+        
+        patcher.applyLookupPatch(&movPatch);
+        
+        if (patcher.getError() != KernelPatcher::Error::NoError) {
+            SYSLOG("ECE", "Failed to apply ecSpaceHandler MOV patch");
+            patcher.clearError();
+        }
+        
+    } else if (index == kextList[1].loadIndex) {
+        KernelPatcher::RouteRequest requests[] = {
+            {"_AcpiExFieldDatumIo", fieldDatumIO, orgFieldDatumIO},
+            {"_AcpiExExtractFromField", extractFromField, orgExtractFromField}
+        };
+        
+        if (!patcher.routeMultiple(index, requests, arrsize(requests), address  , size)) {
+            SYSLOG("ECE", "patcher.routeMultiple for %s failed with error %d", requests[0].symbol, patcher.getError());
+            patcher.clearError();
+        }
     }
+}
+
+IOReturn ECE::fieldDatumIO(union acpi_operand_object *obj_desc, UInt32 offset, unsigned long *values,int is_write)
+{
+    DBGLOG("ECE", "FieldDatumIO Called! Access-Byte-Width: %x Bit-Length: %x Offset: %x", obj_desc->field.access_byte_width, obj_desc->field.bit_length, offset);
+    return FunctionCast(fieldDatumIO, callbackECE->orgFieldDatumIO) (obj_desc, offset, values, is_write);
+}
+
+IOReturn ECE::extractFromField(union acpi_operand_object *obj_desc, void *buffer, UInt32 buffer_length)
+{
+    
+    IOReturn val = FunctionCast(extractFromField, callbackECE->orgExtractFromField) (
+                                                                             obj_desc,
+                                                                             buffer,
+                                                                             buffer_length
+                                                                             );
+    
+    DBGLOG("ECE", "Extract From Field - Buffer Length: 0x%x Type: %x StartFieldBit: %x", buffer_length, obj_desc->common.type, obj_desc->field.start_field_bit_offset);
+    if (buffer_length > 0x8) {
+        DBGLOG("ECE", "!!!!!!!!!! BYTE ALLOCATION SIZE: %lu !!!!!!!!!!!!!!!!!!", *((long *)(buffer) - 1));
+    }
+    return val;
 }
 
 /**
@@ -43,10 +98,11 @@ void ECE::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t ad
  *
  * TODO: Possibly get IOCommandGate from AppleACPIEC to run our own actions
  */
-IOReturn ECE::ecSpaceHandler(unsigned int write, unsigned long addr, unsigned int bits, unsigned char *values64, void *handlerContext, void *RegionContext)
+IOReturn ECE::ecSpaceHandler(UInt32 write, UInt64 addr, UInt32 bits, UInt8 *values64, void *handlerContext, void *RegionContext)
 {
+    int maxAddr = 0x100 - (bits / 8);
     IOReturn result = 0;
-    if (addr >= 0x100 || values64 == nullptr || handlerContext == nullptr) {
+    if (addr > maxAddr || values64 == nullptr || handlerContext == nullptr) {
         return AE_BAD_PARAMETER;
     }
     
@@ -64,15 +120,20 @@ IOReturn ECE::ecSpaceHandler(unsigned int write, unsigned long addr, unsigned in
     } else {
         int bytes = bits / 8;
         
-        // Do not error out, as this breaks battery methods and prevents status from being read.
-        // Set a sane value though
         if (bytes > 8) {
-            *values64 = 0;
-            return kIOReturnSuccess;
+//            bytes = 8;
+            DBGLOG("ECE", "Buffer Size = %lu", *((long *) (values64) - 1));
         }
         
+        // Do not error out, as this breaks battery methods and prevents status from being read.
+        // Set a sane value though
+//        if (bytes > 8) {
+//            *values64 = 0;
+//            return kIOReturnSuccess;
+//        }
+        
         // Read up to a long (8 bytes), 1 byte at a time
-        int maxOffset = min(8, bytes);
+        int maxOffset = bytes;
         int index = 0;
         do {
             result = FunctionCast(ecSpaceHandler, callbackECE->orgACPIEC_ecSpaceHandler) (
--- ECEnabler/ECEnabler.hpp
+++ ECEnabler/ECEnabler.hpp
@@ -5,6 +5,43 @@
 
 #define AE_BAD_PARAMETER 0x1001
 
+#pragma pack(8)
+
+#define ACPI_OBJECT_COMMON_HEADER \
+    union acpi_operand_object *next_object; \
+    UInt8 descriptor_type; \
+    UInt8 type; \
+    UInt16 reference_count; \
+    UInt8 flags; \
+
+struct acpi_object_common {
+    ACPI_OBJECT_COMMON_HEADER
+};
+
+struct acpi_object_field {
+    ACPI_OBJECT_COMMON_HEADER
+    UInt8 field_flags;
+    UInt8 attribute;
+    UInt8 access_byte_width;
+    void *node;
+    UInt32 bit_length;
+    UInt32 base_byte_offset;
+    UInt32 value;
+    UInt8 start_field_bit_offset;
+    UInt8 access_length;
+};
+
+static_assert(sizeof(acpi_object_common) == 16, "Invalid acpi_object_common size");
+static_assert(sizeof(acpi_object_field) == 40, "Invalid acpi_object_field size");
+
+// From Linux/drivers/acpi/acpica/acobject.h
+union acpi_operand_object {
+    struct acpi_object_common common;
+    struct acpi_object_field field;
+};
+
+#pragma pack()
+
 class ECE {
 public:
     void init();
@@ -13,9 +50,13 @@ public:
 private:
     void processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t addres, size_t size);
     
-    static IOReturn ecSpaceHandler(unsigned int, unsigned long, unsigned int, unsigned char *, void*, void *);
+    static IOReturn ecSpaceHandler(UInt32, UInt64, UInt32, UInt8 *, void*, void *);
+    static IOReturn extractFromField(union acpi_operand_object *, void *, UInt32);
+    static IOReturn fieldDatumIO(union acpi_operand_object *, UInt32, unsigned long * ,int);
     
     mach_vm_address_t orgACPIEC_ecSpaceHandler {0};
+    mach_vm_address_t orgExtractFromField {0};
+    mach_vm_address_t orgFieldDatumIO {0};
     
 };
 
